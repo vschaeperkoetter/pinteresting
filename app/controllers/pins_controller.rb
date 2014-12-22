f8ae70d2ed9fd9f153d27b71e13055fed55a93b0
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2014-12-21 22:36:24 -0500

    refactored pins controller. unnecessary code generated from scaffold commented out

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,16 +5,15 @@ class PinsController < ApplicationController
 
   def index
     @pins = Pin.all
-    respond_with(@pins)
+    # respond_with(@pins)
   end
 
   def show
-    respond_with(@pin)
+    # respond_with(@pin)
   end
 
   def new
     @pin = Pin.new
-    respond_with(@pin)
   end
 
   def edit
@@ -22,18 +21,23 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
+    # @pin.save
+    # respond_with(@pin)
   end
 
   def update
     @pin.update(pin_params)
-    respond_with(@pin)
+    # respond_with(@pin)
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    # respond_with(@pin)
   end
 
   private
