c668b94a582b46aba9843a7361794b09fffbf6d3
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2014-12-26 00:01:07 -0500

    modified pins controller. current_user method added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,7 +8,6 @@ class PinsController < ApplicationController
   end
 
   def show
-    # respond_with(@pin)
   end
 
   def new
@@ -19,29 +18,35 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
       render action: 'new'
     end
-    # @pin.save
-    # respond_with(@pin)
   end
 
   def update
-    @pin.update(pin_params)
-    # respond_with(@pin)
+    @if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-    # respond_with(@pin)
+    redirect_to pins_url
   end
 
   private
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     def pin_params
