74440f9c3710c2de2b68c77a12e6df8f88e3afe5
Vic Schaeperkoetter <victor.schaeperkoetter@gmail.com>
2015-01-05 01:44:06 -0500

    shows user's name on pin#index and pin#show pages

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,8 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-
+	has_attached_file :image, :styles => {  :large => "500x500>", 
+                          								:medium => "300x300>", 
+                          								:thumb => "100x100>" }
 	validates :image, presence: true
   validates :description, presence: true
 end
